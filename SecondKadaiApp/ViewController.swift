289a4a30161e91ffff631de46675b41fdfdca243
Na2key <na2key@icloud.com>
2020-09-02 20:27:02 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 山本 夏紀 on 2020/09/02.
+//  Copyright © 2020 natsuki.yamamoto2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
